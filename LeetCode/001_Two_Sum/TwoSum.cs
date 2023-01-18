02188d3df8b4d17ecc9aa84e3134e4d971c147ae
Johan Liljeqvist <johan@jofrelit.net>
2023-01-18 16:50:36 +0100

    Start of a project for coding exercises.

---
+++ LeetCode/001_Two_Sum/TwoSum.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode._001_Two_Sum;
+
+public static class TwoSum {
+    
+    /// <summary>
+    /// Given an array of integers nums and an integer target, return indices of the two numbers
+    /// such that they add up to target. You may assume that each input would have exactly one
+    /// solution, and you may not use the same element twice.
+    ///
+    /// You can return the answer in any order.
+    /// 
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public static int[] TwoSum_BruteForce(int[] nums, int target)
+    {
+        for(var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i+1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+
+            }
+            
+        }
+
+        return null;
+    }
+
+    public static int[]? TwoSum_Dictionary(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            int sum = target - nums[i];
+            if (dict.ContainsKey(sum))
+                return new[] { dict[sum], i };
+            dict[nums[i]] = i;
+        }
+
+        return null;
+    }
+    
+    
+}
---
+++ LeetCode/001_Two_Sum/TwoSumTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._001_Two_Sum;
+
+using NUnit;
+
+[TestFixture]
+public class TwoSumTest
+{
+      
+        
+    [TestCase(new int[] { 3, 2, 4 },6, new int[] { 1, 2 })]
+    [TestCase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [TestCase(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    public void TestBruteForce(int[] nums, int target, int[] solution)
+    {
+        Assert.That(TwoSum.TwoSum_BruteForce(nums, target), Is.EqualTo(solution));
+        
+    }
+
+    [TestCase(new int[] { 3, 2, 4 },6, new int[] { 1, 2 })]
+    [TestCase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [TestCase(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    public void TestDictionary(int[] nums, int target, int[] solution)
+    {
+        Assert.That(TwoSum.TwoSum_Dictionary(nums, target), Is.EqualTo(solution));
+    }
+}
