1b652cad19f5eb934e7e8893f2f80b140f27f346
Johan Liljeqvist <johan@jofrelit.net>
2023-01-19 15:04:35 +0100

    To solutions for the problem: 2. Add Two Numbers.

---
+++ LeetCode/002_Add_Two_Numbers/AddTwoNumbers.cs
@@ -0,0 +1,66 @@
+﻿namespace LeetCode._002_Add_Two_Numbers;
+
+public static class AddTwoNumbers
+{
+    
+
+    public static ListNode AddTwoNumbers_FirstTry(ListNode l1, ListNode l2)
+    {
+        var c1 = l1;
+        var c2 = l2;
+        ListNode resFirstNode = new ListNode();
+        ListNode previous = resFirstNode;
+        ListNode current = resFirstNode;
+        int over = 0;
+        while (c1 != null || c2 != null || over > 0)
+        {
+            int val = (c1?.val ?? 0) + (c2?.val ?? 0) + over;
+            current.val = val % 10;
+            over = val / 10;
+            
+            if(previous != current) previous.next = current;
+            previous = current;
+            current = new ListNode();
+            //Move to next node
+            c1 = c1?.next;
+            c2 = c2?.next;
+            
+        }
+        return resFirstNode;
+    }
+
+
+    public static ListNode AddTwoNumbers_Recursion(ListNode l1, ListNode l2)
+    {
+        ListNode resFirstNode = new ListNode();
+        AddNodesFromLinkedList(l1,l2,resFirstNode);
+        return resFirstNode;
+    }
+
+    private static void AddNodesFromLinkedList(ListNode? l1, ListNode? l2, ListNode node, int over = 0)
+    {
+        int val = (l1?.val ?? 0) + (l2?.val ?? 0) + over;
+        node.val = val % 10;
+        int toNext = val / 10;
+        
+        if (l1?.next != null || l2?.next != null || toNext > 0)
+        {
+            node.next =  new ListNode();
+            AddNodesFromLinkedList(l1?.next, l2?.next, node.next, toNext);
+        }
+    }
+ 
+
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+        
+    public ListNode(int val=0, ListNode next=null) 
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ LeetCode/002_Add_Two_Numbers/AddTwoNumbersTest.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._002_Add_Two_Numbers;
+
+using NUnit;
+
+public class AddTwoNumbersTest
+{
+    public static ListNode l1_1 = new ListNode()
+    {
+        val = 9, next = new ListNode()
+        {
+            val = 9, next = new ListNode()
+            {
+                val = 9, next = new ListNode()
+                {
+                    val = 9, next = new ListNode()
+                    {
+                        val = 9, next = new ListNode()
+                        {
+                            val = 9, next = new ListNode()
+                            {
+                                val = 9
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    
+    public static ListNode l2_1 = new ListNode()
+    {
+        val = 9, next = new ListNode()
+        {
+            val = 9, next = new ListNode()
+            {
+                val = 9, next = new ListNode()
+                {
+                    val = 9
+                }
+            }
+        }
+    };
+    
+    public static ListNode res_1 = new ListNode()
+    {
+        val = 8, next = new ListNode()
+        {
+            val = 9, next = new ListNode()
+            {
+                val = 9, next = new ListNode()
+                {
+                    val = 9, next = new ListNode()
+                    {
+                        val = 0, next = new ListNode()
+                        {
+                            val = 0, next = new ListNode()
+                            {
+                                val = 0, next = new ListNode()
+                                {
+                                    val = 1
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+
+    public static ListNode l1_2 = new ListNode()
+    {
+        val = 0
+    };
+
+    public static ListNode l2_2 = new ListNode()
+    {
+        val = 0
+    };
+    
+    public static ListNode res_2 = new ListNode()
+    {
+        val = 0
+    };
+    
+    
+
+
+    [Test]
+    public void TestAddTwoNumbers_FirstTry()
+    {
+        var resFirstNode1 = AddTwoNumbers.AddTwoNumbers_Recursion(l1_1, l2_1);
+        Assert.That(ListIsEqual(resFirstNode1, res_1));
+        var resFirstNode2 = AddTwoNumbers.AddTwoNumbers_Recursion(l1_2, l2_2);
+        Assert.That(ListIsEqual(resFirstNode2,res_2));
+    }
+    
+    [Test]
+    public void TestAddTwoNumbers_Recursion()
+    {
+        var resFirstNode1 = AddTwoNumbers.AddTwoNumbers_Recursion(l1_1, l2_1);
+        Assert.That(ListIsEqual(resFirstNode1, res_1));
+        var resFirstNode2 = AddTwoNumbers.AddTwoNumbers_Recursion(l1_2, l2_2);
+        Assert.That(ListIsEqual(resFirstNode2,res_2));
+    }
+
+
+    private bool ListIsEqual(ListNode toTry, ListNode expected)
+    {
+        bool isEqual = true;
+        
+        while (isEqual)
+        {
+            if (toTry.val != expected.val)
+                isEqual = false;
+
+            if (toTry.next is null && expected.next is null)
+                break;
+
+            if (toTry.next is null || toTry.next is null)
+            {
+                isEqual = false;
+                break;
+            }
+            
+            toTry = toTry.next;
+            expected = expected.next;
+
+        }
+
+        return isEqual;
+
+    }
+}
