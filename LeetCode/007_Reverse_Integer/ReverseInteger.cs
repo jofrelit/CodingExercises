aab8d21afd29c5b660f15473cd2797dbc982978d
Johan Liljeqvist <johan@jofrelit.net>
2023-01-23 22:24:17 +0100

    First try on Reverse Integer problem.

---
+++ LeetCode/007_Reverse_Integer/ReverseInteger.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode._007_Reverse_Integer;
+
+public class ReverseInteger
+{
+    
+    public int Reverse(int x)
+    {
+        if (x is >= -9 and <= 9) return x;
+        
+        int answer = 0;
+        
+        bool neg = x < 0;
+        if (neg) x = x * -1;
+        
+        int numOfDigits = (int)Math.Log10(x) + 1;
+
+        while (x > 0) 
+        {
+            int lastDigit = x % 10;
+            x = x / 10;
+            try
+            {
+                checked
+                {
+                    answer = (answer * 10) + lastDigit;
+                }
+            }
+            catch (OverflowException e)
+            {
+                return 0;
+            }
+            
+        }
+        answer = !neg ? answer : answer * -1;
+        return answer;
+
+    }
+    
+}
---
+++ LeetCode/007_Reverse_Integer/ReverseIntegerTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._007_Reverse_Integer;
+
+[TestFixture]
+public class ReverseIntegerTest
+{
+    [TestCase(1534236469,0)]
+    [TestCase(321,123)]
+    [TestCase(-123,-321)]
+    [TestCase(120,21)]
+    public void TestReverseInteger(int x, int answer)
+    {
+        Assert.That(new ReverseInteger().Reverse(x), Is.EqualTo(answer));
+    }
+}
